1494b749501829ec22b0c39a9f0625b95ada8589
Darren Leung <leungyatlong1208@gmail.com>
2023-03-05 21:09:51 +0800

    feat: user repository

---
+++ src/main/java/swe/darrenleung/timetrackerauth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package swe.darrenleung.timetrackerauth.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import swe.darrenleung.timetrackerauth.entity.User;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByEmail(String email);
+
+}
