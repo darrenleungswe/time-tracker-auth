e8587121e95d257647c332c6a9d4d2058c0fd8e4
Darren Leung <leungyatlong1208@gmail.com>
2023-03-05 16:36:19 +0800

    Initial commit from Spring Initializr

---
+++ src/main/java/swe/darrenleung/timetrackerauth/TimeTrackerAuthApplication.java
@@ -0,0 +1,13 @@
+package swe.darrenleung.timetrackerauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackerAuthApplication.class, args);
+	}
+
+}
