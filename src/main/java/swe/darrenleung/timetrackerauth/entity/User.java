7b21a7d4f05e46e958b09ae281afe2c9d9782359
Darren Leung <leungyatlong1208@gmail.com>
2023-03-05 17:21:52 +0800

    feat: user entity

---
+++ src/main/java/swe/darrenleung/timetrackerauth/entity/User.java
@@ -0,0 +1,45 @@
+package swe.darrenleung.timetrackerauth.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
